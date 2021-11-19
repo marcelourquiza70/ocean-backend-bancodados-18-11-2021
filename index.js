c1ad5d1afe615fa6ca45a8d00a013f984871ca42
Marcelo Urquiza <76020531+marcelourquiza70@users.noreply.github.com>
2021-11-19 11:23:05 -0300

    alterado url do banco de dados e reativada conexão com o banco

--- index.js
+++ index.js
@@ -7,15 +7,16 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
+//const url = "mongodb://localhost:27017";
+const url = "mongodb+srv://admin:CVM$L8.BsR7nA94@cluster0.r54qd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const dbName = "ocean_bancodados_18_11_2021";
 
 async function main() {
-    /*const client = await MongoClient.connect(url);
+    const client = await MongoClient.connect(url);
 
     const db = client.db(dbName);
 
-    const collection = db.collection("herois");*/
+    const collection = db.collection("herois");
     const collection = undefined;
 
     const app = express();
